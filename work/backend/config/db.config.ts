c201c3478a8c51eb54fb8d93bb756cc5853a45bc
saitooooooo <kitsit.39@gmail.com>
2021-05-23 20:57:18 +0900

    [change]DB接続設定

--- work/backend/config/db.config.ts
+++ work/backend/config/db.config.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 
-dotenv.config({ path: '.env' });
+dotenv.config({ path: __dirname + '/environment/.env' });
 
 interface DatabaseTypes {
   database: string | undefined;
